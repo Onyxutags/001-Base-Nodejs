86215705c71bb416727ef5b7979b75f0514987f3
Onyx.Valencia <200004@utags.edu.mx>
2021-02-18 21:46:15 -0600

    Mi primer commit del proyecto con la multiplicacion completa

---
+++ app.js
@@ -0,0 +1,7 @@
+let base = 3 ;
+
+for(let i=1; i<=10; i++ ){
+//console.log(base + 'x'+ i+'='+ base * i );
+console.log(`${base} x ${i} = ${base * i}`);
+
+}
---
+++ trabajomultiplicacion/multiplicacion.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+
+
+let crearArchivo = (base, limite) => {
+  
+
+return new Promise((resolve, reject) => {
+    let tabla = '';
+    
+    if(!Number(base)){
+       reject('Favor de insertar un numero');
+       return;
+    }
+
+    for(let i=1; i<=limite; i++ ){
+        tabla += `${base} x ${i} = ${base * i}\n`
+        }
+
+     fs.writeFile(`tablas/tabla-${base}.txt`, tabla , (err) => {
+            if (err){ 
+                reject(err)
+            }else{
+            resolve(`tabla-${base}.txt`);
+            }
+
+            });
+    });
+};
+
+
+let listarTabla = (base, limite)=>{
+    return new Promise((resolve, reject)=>{
+  
+        if (!Number(base)){
+          reject('Favor de ingresar un numero');
+          return;
+        }
+  
+        if (!Number(limite)){
+          reject('Favor de ingresar un limite');
+          return;
+        }
+  
+        for (let i=1; i<= limite; i++){
+          console.log( `${base} x ${i} = ${base * i}`);
+          
+        }
+    });
+  };
+
+module.exports = {
+ crearArchivo,
+ listarTabla
+};
